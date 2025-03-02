5b44d21a20ae4b13efc07a702a2b80b918711953
Siddharth <siddhu089@gmail.com>
2025-03-02 10:40:01 +0000

    Added missing port 5000 on url

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function fetchMessage() {
-    fetch("http://13.232.155.200/api/message")
+    fetch("http://13.232.155.200:5000/api/message")
         .then(response => response.json())
         .then(data => {
             document.getElementById("message").innerText = data.message;
